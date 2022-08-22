e5d8cf739403eb1b9e08d3ea9c747ab9a4f01b23
Thet Oo Aung <thetooaung07@gmail.com>
2022-08-22 17:44:26 +0630

    Delete Unused Folders

--- lib/middleware/auth_middleware.dart
+++
@@ -1,7 +0,0 @@
-// import 'package:flutter/cupertino.dart';
-// import 'package:get/get.dart';
-
-// class AuthMiddleware extends GetMiddleware {
-//   @override
-//   RouteSettings? redirect(String? route) => null;
-// }
